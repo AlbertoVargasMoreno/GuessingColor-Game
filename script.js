7f8ef29b5bcb450900a88e642408f4779e14e2eb
Beto <vargasmorenoalberto@gmail.com>
2019-03-07 15:46:28 -0600

    Alerta cuando das click en el correcto

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("hola");
 var colors = [
     "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
@@ -8,9 +7,24 @@ var colors = [
     "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
 var pickedColor = colors[3];
 
+colorDisplay.textContent = pickedColor;
+
 for (var i = 0; i<squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i]; 
+    squares[i].style.backgroundColor = colors[i];
     
+    // add clcik listeners to squares
+    squares[i].addEventListener("click", function(){
+        // grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        // compare color to pickedColor
+        if(clickedColor === pickedColor){
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+    });
+
 }
