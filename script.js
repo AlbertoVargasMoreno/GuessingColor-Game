10ae0d6f15d7b172b3bc45f65fdcc4134eaa04af
Beto <vargasmorenoalberto@gmail.com>
2019-03-07 17:11:36 -0600

    its beautiful

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
@@ -43,4 +36,21 @@ function changeColors(rightColor) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+// cambie el argumento num por howMany
+function generateRandomColors(howMany) {
+    // make array
+    var array = [];
+    // repeat 'howMany' times
+    for(var i=0; i<howMany; i++) {
+        array.push(randomColor());
+    }
+    return array;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
 }
